1856b2e6b698bf527aba9b0041c1435e4707113e
Benjamin LEGROS <benjamin.legros-externe@opendatasoft.com>
2022-12-13 09:40:56 +0100

    Trying to make test PASS

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -6,21 +6,21 @@ from addok.db import DB
 
 
 def test_process_should_index_by_default(factory):
-    doc = factory(skip_index=True, name="Melicocq")
+    doc = factory("Melicocq")
     assert not search("Mélicocq")
     process_documents(json.dumps(doc.copy()))
     assert search("Melicocq")
 
 
 def test_process_should_deindex_if_action_is_given(factory):
-    doc = factory(name="Mélicocq")
+    doc = factory("Mélicocq")
     assert search("Mélicoq")
     process_documents(json.dumps({"_action": "delete", "_id": doc["_id"]}))
     assert not search("Mélicoq")
 
 
 def test_process_should_update_if_action_is_given(factory):
-    doc = factory(name="rue de l'avoine")
+    doc = factory("rue de l'avoine")
     assert search("rue")
     doc["_action"] = "update"
     doc["name"] = "avenue de l'avoine"
@@ -33,7 +33,7 @@ def test_reset(factory, monkeypatch):
     class Args:
         force = False
 
-    factory(name="rue de l'avoine")
+    factory("rue de l'avoine")
     assert DB.keys()
     monkeypatch.setitem(__builtins__, "input", lambda *args, **kwargs: "no")
     reset(Args())
@@ -47,7 +47,7 @@ def test_force_reset(factory):
     class Args:
         force = True
 
-    factory(name="rue de l'avoine")
+    factory("rue de l'avoine")
     assert DB.keys()
     reset(Args())
     assert not DB.keys()
